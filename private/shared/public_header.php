57268f7b623b1809a5b1057cddfac9f48183bdcf
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-09-19 05:16:46 +0000

    ajout de la fonction GUID

---
+++ private/shared/public_header.php
@@ -0,0 +1,14 @@
+<?php
+if(!isset($page_title)) { $page_title = ''; }
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <title>Local Foot - <?php echo htmlspecialchars($page_title); ?></title>
+    <meta charset="utf-8">
+<!--    <link rel="stylesheet" media="all" href="--><?php //echo url_for('/stylesheets'); ?><!--" />-->
+</head>
+
+<body>
