007e9399b4b8c82214484326607b7b24ce2820b8
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-09-19 07:39:41 +0000

    template admin

---
+++ public/admin/reservations.php
@@ -0,0 +1,6 @@
+<?php require_once('../../private/initialize.php');?>
+
+<?php $page_title = 'Terrains' ?>
+<?php include(SHARED_PATH . '/admin_header.php') ?>
+<h1>Coming soon</h1>
+<?php include(SHARED_PATH . '/admin_footer.php') ?>
