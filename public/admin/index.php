0bce3b4d90ccbe4b0e10cf76aba709af5ead49be
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-09-19 07:33:06 +0000

    ajout template admin

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,14 +1,6 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php');?>
 
-<html lang="en">
-<head>
-    <title>LOCAL FOOT</title>
-    <meta charset="utf-8">
-</head>
-
-<body>
-
-<h1>LOCAL FOOT: Coming Soon</h1>
-
-</body>
-</html>
+<?php $page_title = 'Terrains' ?>
+<?php include(SHARED_PATH . '/admin_header.php') ?>
+<h1>Coming soon</h1>
+<?php include(SHARED_PATH . '/admin_footer.php') ?>
