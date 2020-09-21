c60b13c3da8f271bbb7ed454cfe3cf3f0f5c0f21
Alassane Ndoye <aloutndoye.an@gmail.com>
2020-09-21 06:36:15 +0000

    ajout page creer terrain

---
+++ public/admin/new.php
@@ -0,0 +1,12 @@
+<?php require_once('../../private/initialize.php');?>
+
+<?php $page_title = 'Ajouter Terrain' ?>
+<?php include(SHARED_PATH . '/admin_header.php') ?>
+
+
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Ajouter Terrain</h1>
+
+
+
+<?php include(SHARED_PATH . '/admin_footer.php') ?>
